f2dc9489b8dfa060a1aa12422162ec788d45c3ad
Vlasya <64101971+Vlasya@users.noreply.github.com>
2020-10-11 00:31:04 +0300

    done

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-// Есть три тега <input type="text">.
-// Необходимо написать функцию, которая выведет в <textarea> содержимое всех input полей через запятую.
-// Если в `input` через какое-то время произошли изменения – в `textarea` они должны появится тоже.
+// Написать скрипт, который можно выполнить на любой странице в консоли. Скрипт делает следующие действие – убирает значения color, background-color, width, height у всех тэгов на странице, исключая html, head, body.
 
-let userInput=document.querySelectorAll('.userInput'),
-	textarea=document.querySelector('textarea')
 
-let inputs='';
 
-userInput.
-			forEach(el =>{
-				el.addEventListener('change',getInputValue)
-			})
+let tags=['html','head','body'],
+	styles=['color', 'background-color', 'width', 'height']
 
-function getInputValue(e){
-	inputs=e.target.value;
-	textarea.innerHTML+=`${e.target.value} ,`;
+	// ищу все теги в документе
+for (tagDocument of document.all){
+	if(tags.includes(tagDocument.localName))continue
+	console.dir(tagDocument);
+
+	// перебираю все итерируемые стили в теге (нахожу все инлайновые)
+	for(styleTag of tagDocument.style){
+		// перебираю массив со стилями
+		styles.forEach( style=>{
+			
+// если совпадает итерируемы стиль со стилем в массиве,то обнуляю его
+			if(styleTag ===style){
+				console.log('style: ', style);
+				console.log('styleTag: ',styleTag);
+				console.log('tagDocument.style[styleTag]: ', tagDocument.style[styleTag]);
+
+				tagDocument.style[styleTag]=''
+				
+				
+				
+			}
+		})
+	}
 }
+//  но тут  background-color  становится каким-то проклятым, в зависимости от его  порядкогово номера(который прописываю в Диве), он не дает изменить какой-то стиль или не меняется сам(дурачек какой-то). с background не работает.
+				
+				// Получилась рандомная обнулялка
+				// Что со мной не так?)
